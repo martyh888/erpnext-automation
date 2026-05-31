ef3c7fd44fe291e2de082cc1f9bb8d1004d9b561
Nova <nova@mission-control.local>
2026-06-01 03:27:45 +0800

    feat: add sitemap.xml for SEO indexing

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   // Copy static assets
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/sitemap.xml");
 
   // Add date formatting filter
   eleventyConfig.addFilter("dateFormat", (date) => {
